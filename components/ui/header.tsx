aa90fd424b47394e468ea663c276290fdf811dac
LuisCruzA <langel18.1818@gmail.com>
2026-05-30 17:38:37 -0600

    se agreo la pantalla de cart

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -15,10 +15,12 @@ export default function Header() {
 
         {/* Iconos de carrito, campana y Avatar */}
         <div className="flex items-center gap-4 pl-2">
-          <button className="text-slate-700 hover:text-slate-900">
+          <Link href="/dashboard/cart">
+          <button className="text-slate-700 hover:text-slate-900 hover:cursor-pointer">
             {/* Icono Carrito */}
             <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg>
           </button>
+          </Link>
           
           <button className="text-slate-700 hover:text-slate-900">
             {/* Icono Campana */}
